7e8e0d12af5d93ad4859805685e65afa33c7c2bf
alepeluca <orellano442@gmail.com>
2026-06-12 17:39:06 -0300

    Update BASE_URL for licitaciones endpoint

--- scripts/update_licitaciones.py
+++ scripts/update_licitaciones.py
@@ -31,7 +31,7 @@ VERSION = "1.0.0"
 FECHA_MODIFICACION = "12-06-2026"
 
 JSON_CHUNKS_DIR = Path("json_chunks")
-BASE_URL = "https://quilmes.gov.ar"
+BASE_URL = "https://quilmes.gov.ar/contrataciones/licpublicnas/"
 
 HEADERS = {
     "User-Agent": (
